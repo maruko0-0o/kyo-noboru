30fc6a5e9be2232f57d3f9c3d650f703061cf934
maruko0-0o <marukowork@163.com>
2026-08-25 16:58:32 +0900

    Remember group code for installed app launches

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="kyo-noboru-v2";
+const CACHE_NAME="kyo-noboru-v3";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icon.svg","./icon-180.png","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
