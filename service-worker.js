e4160dc268b2585a737038f541a029587e2b3565
maruko0-0o <marukowork@163.com>
2026-08-27 10:23:35 +0900

    Add mobile weekly agenda and onboarding guide

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="kyo-noboru-v5";
+const CACHE_NAME="kyo-noboru-v6";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icon.svg","./icon-180.png","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
