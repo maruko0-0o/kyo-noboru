ef1ec71a56233ae972d46b63df66a417783db0dd
maruko0-0o <marukowork@163.com>
2026-08-27 10:57:19 +0900

    Improve calendar month navigation and day counts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="kyo-noboru-v7";
+const CACHE_NAME="kyo-noboru-v8";
 const APP_SHELL=["./","./index.html","./manifest.webmanifest","./icon.svg","./icon-180.png","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
